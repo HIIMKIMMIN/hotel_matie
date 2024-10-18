f63064148d20d167c55a83a1daeed4781b715651
김민 <santamin118@naver.com>
2024-10-18 12:40:25 +0900

    finish commit

--- js/amenity.js
+++ js/amenity.js
@@ -21,3 +21,34 @@ $(document).ready(function() {
         }
     });
 });
+// 탭 클릭 시 처리
+$('.amenity_tab ul li a').click(function(e) {
+    e.preventDefault(); // 링크 기본 동작 방지
+
+    // 모든 탭에서 'on' 클래스 제거 후 클릭된 탭에 'on' 클래스 추가
+    $('.amenity_tab ul li a').removeClass('on');
+    $(this).addClass('on');
+
+    // 클릭된 탭의 텍스트 가져오기
+    var category = $(this).text().trim();
+
+    // 모든 콘텐츠를 숨김
+    $('.all_contents ul li').hide();
+
+    // '전체' 탭을 눌렀을 때 모든 항목을 표시
+    if (category === '전체') {
+        $('.all_contents ul li').show();
+    }
+    // '다이닝' 탭을 눌렀을 때 .dining 클래스 항목만 표시
+    else if (category === '다이닝') {
+        $('.all_contents ul li.dining').show();
+    }
+    // '편의시설' 탭을 눌렀을 때 .amenity 클래스 항목만 표시
+    else if (category === '편의시설') {
+        $('.all_contents ul li.amenity').show();
+    }
+    // '액티비티' 탭을 눌렀을 때는 아무 것도 표시하지 않음
+    else if (category === '액티비티') {
+        // 아무것도 하지 않음 (모두 숨겨진 상태 유지)
+    }
+});
--- js/matie_main.js
+++ js/matie_main.js
@@ -19,6 +19,30 @@ $(function(){
         },
     });
 
+// jQuery 코드
+$(document).ready(function() {
+    function updateImages() {
+        if ($(window).width() <= 768) {
+            // 화면 크기가 768px 이하일 때
+            $('.swiper-slide img').eq(0).attr('src', 'img/view (3).png');
+            $('.swiper-slide img').eq(1).attr('src', 'img/view (4).png');
+            $('.swiper-slide img').eq(2).attr('src', 'img/view (5).png');
+        } else {
+            // 화면 크기가 768px 이상일 때
+            $('.swiper-slide img').eq(0).attr('src', 'img/view (1).png');
+            $('.swiper-slide img').eq(1).attr('src', 'img/view (2).png');
+            $('.swiper-slide img').eq(2).attr('src', 'img/view.png');
+        }
+    }
+
+    // 페이지가 로드될 때 이미지 업데이트
+    updateImages();
+
+    // 윈도우 리사이즈 시 이미지 업데이트
+    $(window).resize(function() {
+        updateImages();
+    });
+});
 
  
     
